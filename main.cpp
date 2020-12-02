7673435bd54dfe80a38c9984458fd659c1b0e6a5
dimasique <ludaholodec@gmail.com>
2020-12-02 11:36:15 +0400

    final

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     Lexer lexer(jsons_path);
     lexer.load(text_path);
 
-    auto res = lexer.find_lexemes(5);
+    auto res = lexer.find_lexemes(0);
     for (const auto& i : res) {
         std::cout << i << '\n';
     }
